8ff44d0b1f13fcc22d7a736d889be4a3b1c5b06c
MosingiA <ondicho33@gmail.com>
2025-06-08 10:51:46 +0300

    phase-1-arrow-functions

---
+++ index.js
@@ -0,0 +1,10 @@
+let parameter1 = 2000;
+let parameter2 = 100;
+const divide = (parameter1, parameter2) => {
+  console.log(`Deviding ${parameter1}`);
+  console.log(`Deviding ${parameter2}`);
+  return parameter1/parameter2;
+}
+
+const square = num1 => num1*num1;
+const add = (parameter1, parameter2) => (parameter1+parameter2);
